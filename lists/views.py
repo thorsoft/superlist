d7c127d608bd869abae455a38740ff88de55df7d
thp <thp@thorsoft.dk>
2017-11-22 02:39:47 +0100

    Added List to models. Added list as Foreign key in items. Revised unit tests according to the changes

--- lists/views.py
+++ lists/views.py
@@ -2,16 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-    items = Item.objects.all()
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
