f922402a818e0973229f1c16b0af0f3bf74c2d61
thp <thp@thorsoft.dk>
2017-11-22 09:10:38 +0100

    First steps of functional test for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -118,6 +118,29 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Satisfied they both go to sleep
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        input_box.send_keys('Testing')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Testing')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
