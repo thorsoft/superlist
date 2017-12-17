c4540d927d6643ff8b38aba881c60a728e384c77
thp <thp@thorsoft.dk>
2017-12-17 14:40:25 +0100

    Split tests into seperate files.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+
 from lists.models import Item, List
+
 
 class HomePageTest(TestCase):
 
@@ -7,36 +9,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first list item (ever)'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second list item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first list item (ever)')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'The second list item')
-        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
