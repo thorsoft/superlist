34782028381eacd379fc768e6bb94ac7fd9f5bdd
thp <thp@thorsoft.dk>
2017-12-24 16:56:00 +0100

    Adjust new list to view to do model validation

--- lists/models.py
+++ lists/models.py
@@ -5,6 +5,6 @@ class List(models.Model):
     pass
 
 class Item(models.Model):
-    text = models.TextField(default='')
+    text = models.TextField(default='', blank=False)
     list = models.ForeignKey(List, default=None)
 
